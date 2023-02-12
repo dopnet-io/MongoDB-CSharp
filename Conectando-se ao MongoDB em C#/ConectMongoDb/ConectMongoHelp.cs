a8f5366869e4dceed477a5c516b23a8f42571613
Danilo O. Pinheiro <52793184+dopnetskilldev@users.noreply.github.com>
2023-02-12 16:59:54 -0300

    update classe

--- Conectando-se ao MongoDB em C#/ConectMongoDb/ConectMongoHelp.cs
+++ Conectando-se ao MongoDB em C#/ConectMongoDb/ConectMongoHelp.cs
@@ -1,10 +1,15 @@
-var mongoUrl = "mongodb+srv://usermongodbnet:o5mAuvPrT5vTB3Mh@cluster0.fqypfft.mongodb.net/?retryWrites=true&w=majority";
+//using MongoDB.Driver;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Olá, Bem-Vindo ao D@pNetSkillDev!");
 
-var client = new MongoClient(mongoUrl);
-
-var mongoDbList = client.ListDatabases().ToList();
-
-foreach (var itemMongo in mongoDbList)
-{
-    Console.WriteLine(itemMongo);
-}
+//Console.WriteLine("Conectando com MongoDb Atlas - MONGODB CSHARP NET");
+//var mongoUrl = "mongodb+srv://usermongodbnet:<senha>@cluster0.fqypfft.mongodb.net/?retryWrites=true&w=majority";
+//
+//var client = new MongoClient(mongoUrl);
+//
+//var mongoDbList = client.ListDatabases().ToList();
+//
+//foreach (var itemMongo in mongoDbList)
+//{
+//    Console.WriteLine(itemMongo);
+//}
